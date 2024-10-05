102acb802f9248e7935d43bf433d9e9912ca440f
Diego Fernando Guerrero Perez <diego.guerrero9612@gmail.com>
2024-10-04 19:18:39 -0500

    Primer comit del ejercicio proyecto creado pero sin estructura

---
+++ src/main/java/com/diego/spingboot/jpa/project_springboot_jpa_demo/ProjectSpringbootJpaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.diego.spingboot.jpa.project_springboot_jpa_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringbootJpaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringbootJpaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diego/spingboot/jpa/project_springboot_jpa_demo/ProjectSpringbootJpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.spingboot.jpa.project_springboot_jpa_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringbootJpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
